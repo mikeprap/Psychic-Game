99603ba39740b8136b8f760308a7da7f9594c17c
Mike Prap <michaelpraplaski@yahoo.com>
2019-08-29 18:52:14 -0400

    changed display text

--- assets/java/game.js
+++ assets/java/game.js
@@ -7,7 +7,7 @@ var userGuess;
 var computerGuess;
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var displayText = "Are you Psychic....Click any letter to start the game!!!";
+var displayText = "Are you Psychic??? Press any letter to start the game!";
 // create function to display the results to the page
 function updateDisplay() {
     document.querySelector("#displayText").innerHTML = displayText;
